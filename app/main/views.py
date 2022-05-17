eb7d2a047944cc59cff3dbacd96dac14bb484e3c
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-18 00:34:20 +0300

    add  display route for user profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,13 @@
 from flask import render_template,request,redirect,url_for
+from . import main
 
+
+@main.route('/')
+def index():
+    """
+    function to redirect to main page
+    """
+    return render_template('index.html')
 
 @main.route('/userprofile')
 def userprofile():
