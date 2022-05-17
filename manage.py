9d910c34deabdffa0c80dfff69fb5e775e253f10
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-17 16:33:17 +0300

    add initial commit

--- manage.py
+++ manage.py
@@ -1,1 +1,10 @@
-from flask import Flask
+from app import create_app,db
+from flask_script import Manager,Server
+
+app = create_app("development")
+manager = Manager(app)
+
+
+
+if __name__ == "__main__":
+    manager.run()
