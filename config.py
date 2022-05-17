2105cf89252a05c1491734ea10e7d114c8f02a6a
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-17 22:22:41 +0300

    set app configurations

--- config.py
+++ config.py
@@ -2,3 +2,22 @@ import os
 class Config():
     SECRET_KEY = 'secretkey1'
     SQLALCHEMY_DATABASE_URI=''
+    
+    
+class ProdConfig(Config):
+    DEBUG = False
+    
+class DevConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI=''
+    
+class TestingConfig(Config):
+    TESTING = True
+
+
+config_options = {
+'test':TestingConfig,
+'development':DevConfig,
+'production':ProdConfig
+}
