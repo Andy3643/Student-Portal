6d46b0b3d3a033929cdbd0a5dba167bc131f7f3a
limu2030 <limu2030@gmail.com>
2022-05-18 06:22:21 +0300

    create login form using wtf

---
+++ app/main/views.py
@@ -0,0 +1,18 @@
+from flask import render_template,url_for,redirect,flash
+from . import main
+from .forms import RegistrationForm,LoginForm,PitchForm,CommentForm
+from models import User
+from app import db
+from flask_login import login_required, login_user,current_user
+
+
+@main.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user)
+            flash('Thanks for logging in!')
+        return redirect(url_for('main.pitch'))
+    return render_template('login.html',title = 'login',form = form)
